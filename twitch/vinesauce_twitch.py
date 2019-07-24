52724862ac0cb4e52a07d0d81b124b6b534725b6
unex <7575866+unex@users.noreply.github.com>
2019-07-24 15:15:40 +0430

    version motherfuckin 2 boys now with 90% less aids added support for new reddits fancy sidebar widgets

--- twitch/vinesauce_twitch.py
+++ twitch/vinesauce_twitch.py
@@ -1,45 +1,72 @@
-import os, sys
+import os
 import re
 import praw
-import time
 import json
 import requests
-from string import Template
-from datetime import datetime as dt
+import twitch
+from datetime import datetime as dt, timedelta
 from derw import log
 
 TWITCH_ID = os.environ.get("TWITCH_ID")
+WIDGET_ID = os.environ.get("WIDGET_ID")
 
-SUBREDDITS = ['Vinesauce']
-IGNORED_MEMBERS = ['kingky', 'hootey']
+SUBREDDIT = "Vinesauce"
 TEAM_URL = 'http://vinesauce.com/twitch/team-data.json'
 
-USER_URL = "https://api.twitch.tv/kraken/channels/%s?client_id={}".format(TWITCH_ID)
-STREAM_URL = "https://api.twitch.tv/kraken/streams/%s?client_id={}".format(TWITCH_ID)
-HOST_URL = "http://tmi.twitch.tv/hosts?client_id={}&include_logins=1&host=%s".format(TWITCH_ID)
-
-#Streamer template
-TEMPLATE = '>* [$statustext](#$status)[](https://twitch.tv/$name)\n'
-
+team = None
+ttv = twitch.Helix(TWITCH_ID, use_cache=True, cache_duration=timedelta(minutes=10))
 reddit = praw.Reddit('BonziBot', user_agent='Vinesauce Twitch.tv monitor /u/RenegadeAI')
+now = dt.utcnow()
 
-class objdict(dict):
-    def __getattr__(self, name):
-        if name in self:
-            return self[name]
+log.debug('Logged in as {}'.format(reddit.user.me()))
+
+class Stream(object):
+    def __init__(self, user):
+        self.id = user.id
+        self.display_name = user.display_name
+        self.login = user.login
+        self.is_live = user.is_live
+        # self.title = ""
+        self.game = ""
+
+        if user.is_live:
+            self.type = user.stream.type
+            # self.title = user.stream.title
+            self.viewer_count = user.stream.viewer_count
+            self.started_at = user.stream.started_at
+            self.game = ttv.game(id=user.stream.game_id).name
+
         else:
-            raise AttributeError("No such attribute: " + name)
+            # This is still the only way to get hosts I guess
+            r = requests.get(f'http://tmi.twitch.tv/hosts?client_id={TWITCH_ID}&include_logins=1&host={self.id}')
+            if r.status_code == 200:
+                host = r.json().get('hosts')[0]
+                if host.get('target_id'):
+                    self.type = 'hosting'
+                    self.game = f'Hosting {host.get("target_display_name")}'
+                    self.host_target = host.get("target_display_name")
 
-    def __setattr__(self, name, value):
-        self[name] = value
+                    return
 
-    def __delattr__(self, name):
-        if name in self:
-            del self[name]
+            self.type = "offline"
+            if user.login in team:
+                # self.title = team[user.login]["channel"]["status"]
+                self.game = team[user.login]["channel"]["game"]
+
+    def render_sidebar(self):
+        if self.type == "live":
+            status = f'{self.display_name} playing {self.game}'
+        elif self.type == "hosting":
+            status = f'{self.display_name} is now hosting {self.host_target}'
         else:
-            raise AttributeError("No such attribute: " + name)
+            status = f'{self.display_name} last seen playing {self.game}'
 
-# log.debug('Logged in as {}'.format(reddit.user.me()))
+        return f'>* [{status.upper()}](#{self.type})[](https://twitch.tv/{self.login})\n'
+
+    def render_widget(self):
+        status = f'{self.viewer_count}' if self.is_live else self.type
+
+        return f'* [](#{self.type})[**~~{self.display_name}*{status}*~~{self.game.strip()}**](https://twitch.tv/{self.login})\n'
 
 def get_team():
     log.debug('Checking for team updates...')
@@ -60,106 +87,86 @@ def get_team():
         with open("./team.json") as file:
             return json.load(file)
 
-def decode_str(s): return bytes(s, "utf-8").decode("unicode_escape")
+def get_streams(team):
+    log.debug('Fetching streams...')
 
-def render(team):
-    template = Template(decode_str(TEMPLATE))
+    ids = [v["channel"]["_id"] for k, v in team.items()]
+    streams = []
 
-    STREAMING = ''
-    HOSTING = ''
-    OFFLINE = ''
+    for user in ttv.users(ids):
+        stream = Stream(user)
+        streams.append(stream)
 
-    for name, member in team.items():
-        if not name in IGNORED_MEMBERS:
-            for i in range(3):
-                try:
-                    res = requests.get(STREAM_URL % member.get('channel').get('name'))
-                    res.raise_for_status()
+    return streams
 
-                    stream = res.json().get('stream')
-                    if stream:
-                        log.debug('{} is streaming'.format(member.get('channel').get('name')))
-                        STREAMING += template.safe_substitute({
-                                    "name": member.get('channel').get('name'),
-                                    "status": "streaming",
-                                    "statustext": "{0} playing {1}".format(stream.get('channel').get('name'), stream.get('channel').get('game')).upper()
-                                })
 
-                        break
+def update_widget(streams):
+    widget = reddit.subreddit(SUBREDDIT).widgets.items[WIDGET_ID]
+    content = ""
 
-                    else:
-                        res = requests.get(HOST_URL % member.get('channel').get('_id'))
-                        res.raise_for_status()
+    for stream in streams:
+        content += stream.render_widget()
 
-                        host = res.json().get('hosts')[0].get('target_login')
-                        if host:
-                            log.debug('{} is hosting'.format(member.get('channel').get('name')))
-                            HOSTING += template.safe_substitute({
-                                    "name": member.get('channel').get('name'),
-                                    "status": "hosting",
-                                    "statustext": "{0} is now hosting {1}".format(member.get('channel').get('name'), host).upper()
-                                })
+    content += f'\n\n`LAST UPDATED @ {now.strftime("%X")} {now.strftime("%x")} UTC`'
 
-                            break
+    widget.mod.update(text=content, height=24 + len(streams)*50)
 
-                        else:
-                            log.debug('{} is offline'.format(member.get('channel').get('name')))
-                            OFFLINE += template.safe_substitute({
-                                    "name": member.get('channel').get('name'),
-                                    "status": "offline",
-                                    "statustext": "{0[name]} last seen playing {0[game]}".format(member.get('channel')).upper()
-                                })
+    log.debug('updated widget')
 
-                            break
+def update_sidebar(streams):
+    any_live = False
+    first_offline = True
+    content = ""
 
-                except requests.RequestException as e:
-                    log.error("There was an error checking status for {0}, waiting before trying again: {1}".format(name, e))
-                    time.sleep(10)
+    for stream in streams:
+        if stream.type == "live": any_live = True
+        if stream.type == "offline" and first_offline:
+            content += ">* **[](#separator)**\n"
+            first_offline = False
 
-    if (STREAMING or HOSTING) and OFFLINE:
-        OFFLINE = ">* **[](#separator)**\n" + OFFLINE
+        content += stream.render_sidebar()
 
-    title = "> ###CLICK A CHANNEL TO START WATCHING!\n" if STREAMING else "> ###TEAM IS OFFLINE\n"
+    content_header = "> ###CLICK A CHANNEL TO START WATCHING!\n" if any_live else "> ###TEAM IS OFFLINE\n"
 
-    now = dt.utcnow()
-    last_updated = f"* `LAST UPDATED\n@ {now.strftime('%X')}\n{now.strftime('%x')} UTC`\n"
+    content = content_header + content
 
-    return "\n\n{0}{1}{2}\n".format(title, STREAMING + HOSTING + OFFLINE, last_updated)
+    content += f"* `LAST UPDATED\n@ {now.strftime('%X')}\n{now.strftime('%x')} UTC`\n"
 
-def update_sidebar(content):
-    for sub in SUBREDDITS:
-        subreddit = reddit.subreddit(sub)
-        settings = subreddit.mod.settings()
-        sidebar = settings['description']
+    subreddit = reddit.subreddit(SUBREDDIT)
+    settings = subreddit.mod.settings()
+    sidebar = settings['description']
 
-        # Remove text currently between the markers
-        sidebar = re.sub(r'(\[\]\(#BOT_STREAMS\)).*(\[\]\(/BOT_STREAMS\))',
-                        '\\1\\2',
-                        sidebar,
-                        flags=re.DOTALL)
+    # Remove text currently between the markers
+    sidebar = re.sub(r'(\[\]\(#BOT_STREAMS\)).*(\[\]\(/BOT_STREAMS\))',
+                    '\\1\\2',
+                    sidebar,
+                    flags=re.DOTALL)
 
-        # Place new text between the markers
-        opening_marker = "[](#BOT_STREAMS)"
-        if content:
-            try:
-                marker_pos = sidebar.index(opening_marker) + len(opening_marker)
-                sidebar = sidebar[:marker_pos] + content + sidebar[marker_pos:]
+    # Place new text between the markers
+    opening_marker = "[](#BOT_STREAMS)"
+    if content:
+        try:
+            marker_pos = sidebar.index(opening_marker) + len(opening_marker)
+            sidebar = sidebar[:marker_pos] + f'\n\n{content}\n' + sidebar[marker_pos:]
 
-                subreddit.mod.update(description=sidebar)
+            subreddit.mod.update(description=sidebar)
 
-            except ValueError:
-                # Substring not found
-                log.critical("No streams marker found for /r/{}".format(sub))
+        except ValueError:
+            # Substring not found
+            log.critical(f'No streams marker found for /r/{SUBREDDIT}')
 
-        log.debug("Updated /r/{}".format(sub))
+    log.debug('updated sidebar')
 
 if __name__ == '__main__':
     team = get_team()
 
-    content = render(team)
+    streams = get_streams(team)
 
-    try:
-        update_sidebar(content)
+    sort = []
+    for state in ["live", "hosting", "offline"]:
+        for stream in streams:
+            if stream.type == state:
+                sort.append(stream)
 
-    except Exception as e:
-        log.error('Error updating sidebar: {}'.format(e))
+    update_widget(sort)
+    update_sidebar(sort)
